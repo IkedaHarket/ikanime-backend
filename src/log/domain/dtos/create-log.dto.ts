8a25091579637fbf90205066dc78894e6b953342
IkedaHarket <sebaaignacio111@gmail.com>
2024-02-23 09:42:09 -0300

    FIX: create-log.dto.ts

--- src/log/domain/dtos/create-log.dto.ts
+++ src/log/domain/dtos/create-log.dto.ts
@@ -1,33 +1,25 @@
 import { LogSeverityLevel } from "../";
 
-export interface LogEntityDtoProps{
-    message:string;
-    level:LogSeverityLevel;
-    createdAt?: Date;
-    origin:string;
-}
 
 export class CreateLogDto {
 
-    public createdAt : Date;
     public level: LogSeverityLevel;
     public message: string;
     public origin: string;
     
-    constructor({ message, level, origin, createdAt = new Date() }: LogEntityDtoProps){
-        this.createdAt = createdAt;
+    private constructor( message: string , level: LogSeverityLevel , origin: string){
         this.level = level;
         this.message = message;
         this.origin = origin;
     }
 
-    static createFromUnknown(props:{[key:string]:any}): [ string?, CreateLogDto? ] {
-        const { level, message, origin, createdAt = true } = props
-        if(this.isNotValidSeveritylevel(level)) return [`Level is not valid`] 
-        if(!message) return [`Message is required`] 
-        if(!origin) return [`origin is required is required`] 
+    static create(props:{[key:string]:any}): [ string?, CreateLogDto? ] {
+        const { level, message, origin } = props
+        if(this.isNotValidSeveritylevel(level)) return [`level is not valid`] 
+        if(!message) return [`message is required`] 
+        if(!origin) return [`origin is required`] 
         
-        return [undefined, new CreateLogDto({level, message, origin, createdAt})]
+        return [undefined, new CreateLogDto( message, level, origin )]
     }
 
     private static isValidSeverityLevel(severity:string): severity is LogSeverityLevel {
